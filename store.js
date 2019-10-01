f1125c4fd337cd861e47c01fdc457e1370fb8d59
Kimberly Cable <cableredi@comcast.net>
2019-10-01 09:00:46 -0600

    skeleton for different pages

---
+++ store.js
@@ -0,0 +1,77 @@
+const STORE = {
+  questions: [
+    {
+      question: "Also known as the 360, this soccer move uses a spin to create separation from defenders and win yourself some extra time and space.",
+      options: [
+        "The Okocha", "The Roulette", "The Rabona", "The Rainbow"
+      ],
+      answer: [
+        "The Roulette", "Zidane and Maradona often used it and looked effortless when they did."
+    },
+    {
+      question: "To pull off this soccer move successfully, you want to hit the ball with the outside of your foot before immediately changing direction by striking the ball with the inside of your foot and bringing the ball across your body.",
+      options: [
+        "The Elastico", "The Dummy", "The Fake Pull Back", "The Hocus Pocus"
+      ],
+      answer: "The Elastico",
+    },
+    {
+      question: "Show the ball to your opponent and open your body up looking like you’re able to attempt a move to blow past the defender, but instead hook the ball with the interior of your foot and slow it down.",
+      options: [
+        "The Rivelio", "The Nutemeg", "The Inside Hook", "The Aurelio"
+      ],
+      answer: "The Inside Hook",
+    },
+    {
+      question: "You prepare to receive the ball, but let it go through your legs at the last moment to another teammate.",
+      options: [
+        "The Shoulder Feint", "The Dummy", "The Cut", "The Hocus Pocus"
+      ],
+      answer: "The Dummy",
+    },
+    {
+      question: "While sprinting along the flank and when an opposition defender is sprinting alongside you, strike the ball with the inside of your foot and cut it behind your standing leg.",
+      options: [
+        "The Rainbow", "The Roulette", "The Ronaldo Chop", "The Matthews Cut"
+      ],
+      answer: "The Ronaldo Chop",
+    },
+    {
+      question: "With both feet on either side of the ball, use one leg to roll the ball up the back of your other leg before flicking the ball up and over the head of your opponent.",
+      options: [
+        "The Around the World", "The Rabona", "The Hocus Pocus", "The Rainbow"
+      ],
+      answer: "The Rainbow",
+    },
+    {
+      question: "When facing two defenders, hold the ball between both feet and hop through the air, carrying the ball past your opposition.",
+      options: [
+        "The Double Lunge", "The Pull Back", "The Seal Dribble", "The La Cuauhteminha"
+      ],
+      answer: "The La Cuauhteminha",
+    },
+    {
+      question: "Starting with the ball attached to the inside of your foot, scoop the ball behind your standing leg and then push the ball back in front to the same side of your body with the outside of the same foot.",
+      options: [
+        "The Hocus Pocus", "The Rabona", "The Aurelio", "The Rivelino"
+      ],
+      answer: "The Hocus Pocus",
+    },
+    {
+      question: "Place your foot on top of the ball and drag it back towards you as the defender reaches in to knock the ball away.",
+      options: [
+        "The Double Lunge", "The Pullback V", "The Cut", "The Elastico"
+      ],
+      answer: "The Pullback V",
+    },
+    {
+      question: "Once you’ve sold your defender a couple of dummies and they’re retreating flat-footed and unsure of what to do, slip the ball through their legs for the ultimate humiliation.",
+      options: [
+        "The Okocha", "The Rainbow", "The Nutmeg", "The Around the World"
+      ],
+      answer: "The Nutmeg",
+    },
+  ],
+  currentQuestion: 0,
+  score,
+};
